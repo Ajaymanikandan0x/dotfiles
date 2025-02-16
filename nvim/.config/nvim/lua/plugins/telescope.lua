8d8666e324eacb25f34dae15edb848a7009e2161
Ajay Manikandan <ajaymanikandan360@gmail.com>
2025-02-16 20:25:25 +0530

    add search in same file telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -26,6 +26,7 @@ return {
       vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+      vim.keymap.set("n", "<leader>sf", builtin.current_buffer_fuzzy_find, { desc = "[S]earch in current [F]ile" })
 
       require("telescope").load_extension("ui-select")
     end,
